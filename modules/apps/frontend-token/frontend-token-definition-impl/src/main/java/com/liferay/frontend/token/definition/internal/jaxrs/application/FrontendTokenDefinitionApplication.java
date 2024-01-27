8f7d0ef344e423223588d6bcbc4192f0476689d5
Thiago Buarque <thiago.buarque@liferay.com>
2024-01-27 20:34:07 -0300

    LPD-7079 add validation for FrontendTokenDefinition

---
+++ modules/apps/frontend-token/frontend-token-definition-impl/src/main/java/com/liferay/frontend/token/definition/internal/jaxrs/application/FrontendTokenDefinitionApplication.java
@@ -0,0 +1,218 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.frontend.token.definition.internal.jaxrs.application;
+
+import com.liferay.frontend.token.definition.FrontendToken;
+import com.liferay.frontend.token.definition.FrontendTokenCategory;
+import com.liferay.frontend.token.definition.FrontendTokenDefinition;
+import com.liferay.frontend.token.definition.FrontendTokenDefinitionRegistry;
+import com.liferay.frontend.token.definition.FrontendTokenSet;
+import com.liferay.frontend.token.definition.internal.FrontendTokenDefinitionImpl;
+import com.liferay.frontend.token.definition.internal.validator.FrontendTokenDefinitionValidator;
+import com.liferay.portal.json.validator.JSONValidatorException;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
+import com.liferay.portal.kernel.json.JSONException;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.upload.UploadServletRequest;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.StringUtil;
+
+import java.io.File;
+import java.io.IOException;
+
+import java.nio.file.Files;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Locale;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.jaxrs.whiteboard.JaxrsWhiteboardConstants;
+
+/**
+ * @author Anderson Luiz
+ * @author Thiago Buarque
+ */
+@Component(
+	property = {
+		JaxrsWhiteboardConstants.JAX_RS_APPLICATION_BASE + "=/frontend-token-definition",
+		JaxrsWhiteboardConstants.JAX_RS_NAME + "=com.liferay.frontend.token.definition.internal.jaxrs.application.FrontendTokenDefinitionApplication",
+		"auth.verifier.auth.verifier.PortalSessionAuthVerifier.urls.includes=/*",
+		"auth.verifier.guest.allowed=false", "liferay.oauth2=false"
+	},
+	service = Application.class
+)
+public class FrontendTokenDefinitionApplication extends Application {
+
+	@Path("/tokens-info")
+	@POST
+	public Response getFrontendTokenDefinitionTokensInfo(
+		@Context HttpServletRequest httpServletRequest,
+		@FormParam("companyId") long companyId,
+		@FormParam("externalReferenceCode") String externalReferenceCode) {
+
+		FrontendTokenDefinition frontendTokenDefinition =
+			_frontendTokenDefinitionRegistry.getFrontendTokenDefinition(
+				companyId, externalReferenceCode, null);
+
+		if (frontendTokenDefinition != null) {
+			return _getFrontendTokenDefinitionTokensInfo(
+				_portal.getLocale(httpServletRequest), frontendTokenDefinition);
+		}
+
+		return Response.ok(
+		).build();
+	}
+
+	public Set<Object> getSingletons() {
+		return Collections.singleton(this);
+	}
+
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	@Path("/validate-file")
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response validateFrontendTokenDefinitionFile(
+		@Context HttpServletRequest httpServletRequest) {
+
+		if (!FeatureFlagManagerUtil.isEnabled("LPD-10773")) {
+			return Response.serverError(
+			).build();
+		}
+
+		UploadServletRequest uploadServletRequest =
+			_portal.getUploadServletRequest(httpServletRequest);
+
+		Locale locale = _portal.getLocale(httpServletRequest);
+
+		try {
+			return _validateFrontendTokenDefinitionFile(
+				uploadServletRequest.getFile("file"), locale);
+		}
+		catch (IOException ioException) {
+			_log.error(ioException);
+
+			return _getResponse(
+				_language.get(
+					locale,
+					"your-upload-failed-to-complete-please-try-again-or-" +
+						"contact-support-if-the-error-persists"),
+				Response.Status.INTERNAL_SERVER_ERROR);
+		}
+		catch (JSONException | JSONValidatorException exception) {
+			_log.error(exception);
+
+			return _getResponse(
+				_language.get(
+					locale,
+					"the-format-is-not-valid-please-upload-a-valid-frontend-" +
+						"token-definition-json-file"),
+				Response.Status.BAD_REQUEST);
+		}
+	}
+
+	private Response _getFrontendTokenDefinitionTokensInfo(
+		Locale locale, FrontendTokenDefinition frontendTokenDefinition) {
+
+		Collection<FrontendTokenCategory> frontendTokenCategories =
+			frontendTokenDefinition.getFrontendTokenCategories();
+		Collection<FrontendTokenSet> frontendTokenSets =
+			frontendTokenDefinition.getFrontendTokenSets();
+		Collection<FrontendToken> frontendTokens =
+			frontendTokenDefinition.getFrontendTokens();
+
+		return _getResponse(
+			_language.format(
+				locale,
+				"frontend-token-definition-uploaded-contributing-x-token-" +
+					"categories-x-token-sets-and-x-tokens",
+				new Object[] {
+					frontendTokenCategories.size(), frontendTokenSets.size(),
+					frontendTokens.size()
+				}),
+			Response.Status.OK);
+	}
+
+	private Response _getResponse(String message, Response.Status status) {
+		return Response.status(
+			status
+		).entity(
+			HashMapBuilder.put(
+				"message", message
+			).build()
+		).type(
+			MediaType.APPLICATION_JSON
+		).build();
+	}
+
+	private Response _validateFrontendTokenDefinitionFile(
+			File file, Locale locale)
+		throws IOException, JSONException, JSONValidatorException {
+
+		if ((file == null) || !StringUtil.endsWith(file.getName(), ".json")) {
+			return _getResponse(
+				_language.get(locale, "please-upload-a-json-file"),
+				Response.Status.BAD_REQUEST);
+		}
+
+		String frontendTokenDefinitionJSONString = "";
+
+		if (file.exists()) {
+			frontendTokenDefinitionJSONString = new String(
+				Files.readAllBytes(file.toPath()));
+		}
+
+		_frontendTokenDefinitionValidator.validateFrontendTokenDefinition(
+			frontendTokenDefinitionJSONString);
+
+		FrontendTokenDefinition frontendTokenDefinition =
+			new FrontendTokenDefinitionImpl(
+				_jsonFactory.createJSONObject(
+					frontendTokenDefinitionJSONString),
+				_jsonFactory, null, "");
+
+		return _getFrontendTokenDefinitionTokensInfo(
+			locale, frontendTokenDefinition);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		FrontendTokenDefinitionApplication.class);
+
+	@Reference
+	private FrontendTokenDefinitionRegistry _frontendTokenDefinitionRegistry;
+
+	private final FrontendTokenDefinitionValidator
+		_frontendTokenDefinitionValidator =
+			new FrontendTokenDefinitionValidator();
+
+	@Reference
+	private JSONFactory _jsonFactory;
+
+	@Reference
+	private Language _language;
+
+	@Reference
+	private Portal _portal;
+
+}
