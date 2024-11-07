664977b92bc2237fc35ab9417da133f63d302aff
Raymond Augé <raymond.auge@liferay.com>
2024-11-07 12:21:49 -0500

    LPD-41528 generate

--- modules/apps/portal-language/portal-language-rest-api/src/main/java/com/liferay/portal/language/rest/resource/v1_0/MessageResource.java
+++ modules/apps/portal-language/portal-language-rest-api/src/main/java/com/liferay/portal/language/rest/resource/v1_0/MessageResource.java
@@ -19,6 +19,7 @@ import com.liferay.portal.vulcan.accept.language.AcceptLanguage;
 import com.liferay.portal.vulcan.batch.engine.resource.VulcanBatchEngineExportTaskResource;
 import com.liferay.portal.vulcan.batch.engine.resource.VulcanBatchEngineImportTaskResource;
 import com.liferay.portal.vulcan.multipart.MultipartBody;
+import com.liferay.portal.vulcan.pagination.Page;
 
 import java.util.Collections;
 import java.util.List;
@@ -63,6 +64,10 @@ public interface MessageResource {
 	public Message putMessage(Message message) throws Exception;
 
 	public Response putMessageBatch(String callbackURL, Object object)
+		throws Exception;
+
+	public Page<Message> postMessagesExportPage(
+			String languageId, String[] strings)
 		throws Exception;
 
 	public void postMessageImport(
