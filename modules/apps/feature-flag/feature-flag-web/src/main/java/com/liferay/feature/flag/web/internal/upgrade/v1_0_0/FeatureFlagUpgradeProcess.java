1a515a4fcc2a4feb633428c98ead77a90e92f0ee
Thiago Buarque <thiago.buarque@liferay.com>
2025-02-06 20:54:57 -0300

    LPD-41336 disabled deprecation feature flags for the system when initializing Liferay for the first time

--- modules/apps/feature-flag/feature-flag-web/src/main/java/com/liferay/feature/flag/web/internal/upgrade/v1_0_0/FeatureFlagUpgradeProcess.java
+++ modules/apps/feature-flag/feature-flag-web/src/main/java/com/liferay/feature/flag/web/internal/upgrade/v1_0_0/FeatureFlagUpgradeProcess.java
@@ -6,6 +6,7 @@
 package com.liferay.feature.flag.web.internal.upgrade.v1_0_0;
 
 import com.liferay.portal.kernel.feature.flag.constants.FeatureFlagConstants;
+import com.liferay.portal.kernel.model.CompanyConstants;
 import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.service.PortalPreferencesLocalService;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
@@ -30,41 +31,42 @@ public class FeatureFlagUpgradeProcess extends UpgradeProcess {
 
 	@Override
 	protected void doUpgrade() {
-		_companyLocalService.forEachCompanyId(
-			companyId -> {
-				PortalPreferencesWrapper portalPreferencesWrapper =
-					(PortalPreferencesWrapper)
-						_portalPreferencesLocalService.getPreferences(
-							companyId, PortletKeys.PREFS_OWNER_TYPE_COMPANY);
+		_companyLocalService.forEachCompanyId(this::_doUpgrade);
 
-				PortalPreferencesImpl portalPreferencesImpl =
-					portalPreferencesWrapper.getPortalPreferencesImpl();
+		_doUpgrade(CompanyConstants.SYSTEM);
+	}
 
-				Enumeration<String> enumeration =
-					portalPreferencesImpl.getNames(_OLD_NAMESPACE);
+	private void _doUpgrade(long companyId) {
+		PortalPreferencesWrapper portalPreferencesWrapper =
+			(PortalPreferencesWrapper)
+				_portalPreferencesLocalService.getPreferences(
+					companyId, PortletKeys.PREFS_OWNER_TYPE_COMPANY);
 
-				while (enumeration.hasMoreElements()) {
-					String featureFlag = enumeration.nextElement();
+		PortalPreferencesImpl portalPreferencesImpl =
+			portalPreferencesWrapper.getPortalPreferencesImpl();
 
-					String value = portalPreferencesImpl.getValue(
-						_OLD_NAMESPACE, featureFlag);
+		Enumeration<String> enumeration = portalPreferencesImpl.getNames(
+			_OLD_NAMESPACE);
 
-					portalPreferencesImpl.reset(_OLD_NAMESPACE, featureFlag);
+		while (enumeration.hasMoreElements()) {
+			String featureFlag = enumeration.nextElement();
 
-					portalPreferencesImpl.setValue(
-						FeatureFlagConstants.PREFERENCE_NAMESPACE, featureFlag,
-						value);
-				}
+			String value = portalPreferencesImpl.getValue(
+				_OLD_NAMESPACE, featureFlag);
 
-				portalPreferencesImpl.setValue(
-					FeatureFlagConstants.PREFERENCE_NAMESPACE,
-					FeatureFlagConstants.PREFERENCE_KEY_DEPRECATION_PROCESSED,
-					"true");
+			portalPreferencesImpl.reset(_OLD_NAMESPACE, featureFlag);
 
-				_portalPreferencesLocalService.updatePreferences(
-					companyId, PortletKeys.PREFS_OWNER_TYPE_COMPANY,
-					portalPreferencesImpl);
-			});
+			portalPreferencesImpl.setValue(
+				FeatureFlagConstants.PREFERENCE_NAMESPACE, featureFlag, value);
+		}
+
+		portalPreferencesImpl.setValue(
+			FeatureFlagConstants.PREFERENCE_NAMESPACE,
+			FeatureFlagConstants.PREFERENCE_KEY_DEPRECATION_PROCESSED, "true");
+
+		_portalPreferencesLocalService.updatePreferences(
+			companyId, PortletKeys.PREFS_OWNER_TYPE_COMPANY,
+			portalPreferencesImpl);
 	}
 
 	private static final String _OLD_NAMESPACE =
