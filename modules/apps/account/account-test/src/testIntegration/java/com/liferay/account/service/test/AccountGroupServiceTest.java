906a3f0d4bcaf7caa1168f5937620b6985818d89
Stefano Motta <117469730+smottal@users.noreply.github.com>
2025-05-29 09:18:27 +0200

    LPD-55587 Integration test

--- modules/apps/account/account-test/src/testIntegration/java/com/liferay/account/service/test/AccountGroupServiceTest.java
+++ modules/apps/account/account-test/src/testIntegration/java/com/liferay/account/service/test/AccountGroupServiceTest.java
@@ -12,15 +12,18 @@ import com.liferay.account.service.AccountGroupService;
 import com.liferay.account.service.test.util.AccountGroupTestUtil;
 import com.liferay.account.service.test.util.UserRoleTestUtil;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.petra.lang.SafeCloseable;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.lazy.referencing.LazyReferencingThreadLocal;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.search.BaseModelSearchResult;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
 import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.context.ContextUserReplace;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
-import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.PortletKeys;
@@ -29,9 +32,7 @@ import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
 import java.util.List;
 
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -39,6 +40,7 @@ import org.junit.runner.RunWith;
 
 /**
  * @author Pei-Jung Lan
+ * @author Stefano Motta
  */
 @RunWith(Arquillian.class)
 public class AccountGroupServiceTest {
@@ -48,57 +50,132 @@ public class AccountGroupServiceTest {
 	public static final LiferayIntegrationTestRule liferayIntegrationTestRule =
 		new LiferayIntegrationTestRule();
 
-	@Before
-	public void setUp() throws Exception {
-		_user = UserTestUtil.addUser();
-
-		UserTestUtil.setUser(_user);
-	}
-
-	@After
-	public void tearDown() throws Exception {
-		UserTestUtil.setUser(TestPropsValues.getUser());
-	}
-
 	@Test
 	public void testAddAccountGroup() throws Exception {
+
+		// With resource permission
+
+		User user = UserTestUtil.addUser();
+
 		UserRoleTestUtil.addResourcePermission(
 			AccountActionKeys.ADD_ACCOUNT_GROUP, PortletKeys.PORTAL,
-			_user.getUserId());
+			user.getUserId());
 
-		_accountGroupService.addAccountGroup(
-			StringPool.BLANK, _user.getUserId(), RandomTestUtil.randomString(),
-			RandomTestUtil.randomString(), new ServiceContext());
-	}
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				user, PermissionCheckerFactoryUtil.create(user))) {
 
-	@Test(expected = PrincipalException.class)
-	public void testAddAccountGroupWithoutPermission() throws Exception {
-		_accountGroupService.addAccountGroup(
-			StringPool.BLANK, _user.getUserId(), RandomTestUtil.randomString(),
-			RandomTestUtil.randomString(), new ServiceContext());
+			AccountGroup accountGroup = _accountGroupService.addAccountGroup(
+				StringPool.BLANK, user.getUserId(),
+				RandomTestUtil.randomString(), RandomTestUtil.randomString(),
+				new ServiceContext());
+
+			Assert.assertNotNull(accountGroup);
+		}
+
+		// Without resource permission
+
+		user = UserTestUtil.addUser();
+
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				user, PermissionCheckerFactoryUtil.create(user))) {
+
+			_accountGroupService.addAccountGroup(
+				StringPool.BLANK, user.getUserId(),
+				RandomTestUtil.randomString(), RandomTestUtil.randomString(),
+				new ServiceContext());
+
+			Assert.fail();
+		}
+		catch (PrincipalException.MustHavePermission principalException) {
+			Assert.assertNotNull(principalException);
+		}
 	}
 
 	@Test
 	public void testDeleteAccountGroup() throws Exception {
-		AccountGroup accountGroup = AccountGroupTestUtil.addAccountGroup(
-			_accountGroupLocalService, RandomTestUtil.randomString(),
-			RandomTestUtil.randomString());
+
+		// With resource permission
+
+		User user = UserTestUtil.addUser();
 
 		UserRoleTestUtil.addResourcePermission(
-			ActionKeys.DELETE, AccountGroup.class.getName(), _user.getUserId());
+			ActionKeys.DELETE, AccountGroup.class.getName(), user.getUserId());
 
-		_accountGroupService.deleteAccountGroup(
-			accountGroup.getAccountGroupId());
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				user, PermissionCheckerFactoryUtil.create(user))) {
+
+			AccountGroup accountGroup = AccountGroupTestUtil.addAccountGroup(
+				_accountGroupLocalService, RandomTestUtil.randomString(),
+				RandomTestUtil.randomString());
+
+			accountGroup = _accountGroupService.deleteAccountGroup(
+				accountGroup.getAccountGroupId());
+
+			Assert.assertNotNull(accountGroup);
+		}
+
+		// Without resource permission
+
+		user = UserTestUtil.addUser();
+
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				user, PermissionCheckerFactoryUtil.create(user))) {
+
+			AccountGroup accountGroup = AccountGroupTestUtil.addAccountGroup(
+				_accountGroupLocalService, RandomTestUtil.randomString(),
+				RandomTestUtil.randomString());
+
+			_accountGroupService.deleteAccountGroup(
+				accountGroup.getAccountGroupId());
+
+			Assert.fail();
+		}
+		catch (PrincipalException.MustHavePermission principalException) {
+			Assert.assertNotNull(principalException);
+		}
 	}
 
-	@Test(expected = PrincipalException.class)
-	public void testDeleteAccountGroupWithoutPermission() throws Exception {
-		AccountGroup accountGroup = AccountGroupTestUtil.addAccountGroup(
-			_accountGroupLocalService, RandomTestUtil.randomString(),
-			RandomTestUtil.randomString());
+	@Test
+	public void testGetOrAddIncompleteAccountGroup() throws Exception {
+		try (SafeCloseable safeCloseable =
+				LazyReferencingThreadLocal.setEnabledWithSafeCloseable(true)) {
 
-		_accountGroupService.deleteAccountGroup(
-			accountGroup.getAccountGroupId());
+			// With resource permission
+
+			User user = UserTestUtil.addUser();
+
+			UserRoleTestUtil.addResourcePermission(
+				AccountActionKeys.ADD_ACCOUNT_GROUP, PortletKeys.PORTAL,
+				user.getUserId());
+
+			try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+					user, PermissionCheckerFactoryUtil.create(user))) {
+
+				AccountGroup accountGroup =
+					_accountGroupService.getOrAddIncompleteAccountGroup(
+						RandomTestUtil.randomString(),
+						RandomTestUtil.randomString());
+
+				Assert.assertNotNull(accountGroup);
+			}
+
+			// Without resource permission
+
+			user = UserTestUtil.addUser();
+
+			try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+					user, PermissionCheckerFactoryUtil.create(user))) {
+
+				_accountGroupService.getOrAddIncompleteAccountGroup(
+					RandomTestUtil.randomString(),
+					RandomTestUtil.randomString());
+
+				Assert.fail();
+			}
+			catch (PrincipalException.MustHavePermission principalException) {
+				Assert.assertNotNull(principalException);
+			}
+		}
 	}
 
 	@Test
@@ -110,78 +187,101 @@ public class AccountGroupServiceTest {
 			_accountGroupLocalService, RandomTestUtil.randomString(),
 			RandomTestUtil.randomString());
 
+		// With resource permission
+
+		User user = UserTestUtil.addUser();
+
 		UserRoleTestUtil.addResourcePermission(
-			ActionKeys.VIEW, AccountGroup.class.getName(), _user.getUserId());
+			ActionKeys.VIEW, AccountGroup.class.getName(), user.getUserId());
 
-		BaseModelSearchResult<AccountGroup> baseModelSearchResult =
-			_accountGroupService.searchAccountGroups(
-				_user.getCompanyId(), null, QueryUtil.ALL_POS,
-				QueryUtil.ALL_POS, null);
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				user, PermissionCheckerFactoryUtil.create(user))) {
 
-		List<AccountGroup> expectedAccountGroups =
-			_accountGroupLocalService.getAccountGroups(
-				_user.getCompanyId(), QueryUtil.ALL_POS, QueryUtil.ALL_POS,
-				null);
+			List<AccountGroup> expectedAccountGroups = ListUtil.filter(
+				_accountGroupLocalService.getAccountGroups(
+					user.getCompanyId(), QueryUtil.ALL_POS, QueryUtil.ALL_POS,
+					null),
+				accountGroup -> !accountGroup.isDefaultAccountGroup());
 
-		expectedAccountGroups = ListUtil.filter(
-			expectedAccountGroups,
-			accountGroup -> !accountGroup.isDefaultAccountGroup());
+			BaseModelSearchResult<AccountGroup> baseModelSearchResult =
+				_accountGroupService.searchAccountGroups(
+					user.getCompanyId(), null, QueryUtil.ALL_POS,
+					QueryUtil.ALL_POS, null);
 
-		Assert.assertEquals(
-			expectedAccountGroups.size(), baseModelSearchResult.getLength());
+			Assert.assertEquals(
+				expectedAccountGroups.size(),
+				baseModelSearchResult.getLength());
 
-		List<AccountGroup> accountGroups =
-			baseModelSearchResult.getBaseModels();
+			List<AccountGroup> accountGroups =
+				baseModelSearchResult.getBaseModels();
 
-		for (AccountGroup expectedAccountGroup : expectedAccountGroups) {
-			Assert.assertTrue(accountGroups.contains(expectedAccountGroup));
+			Assert.assertTrue(accountGroups.containsAll(expectedAccountGroups));
+		}
+
+		// Without resource permission
+
+		user = UserTestUtil.addUser();
+
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				user, PermissionCheckerFactoryUtil.create(user))) {
+
+			BaseModelSearchResult<AccountGroup> baseModelSearchResult =
+				_accountGroupService.searchAccountGroups(
+					user.getCompanyId(), null, QueryUtil.ALL_POS,
+					QueryUtil.ALL_POS, null);
+
+			Assert.assertEquals(0, baseModelSearchResult.getLength());
+			Assert.assertTrue(
+				ListUtil.isEmpty(baseModelSearchResult.getBaseModels()));
 		}
 	}
 
 	@Test
-	public void testSearchAccountGroupsWithoutPermission() throws Exception {
-		AccountGroupTestUtil.addAccountGroup(
-			_accountGroupLocalService, RandomTestUtil.randomString(),
-			RandomTestUtil.randomString());
-		AccountGroupTestUtil.addAccountGroup(
-			_accountGroupLocalService, RandomTestUtil.randomString(),
-			RandomTestUtil.randomString());
-
-		BaseModelSearchResult<AccountGroup> baseModelSearchResult =
-			_accountGroupService.searchAccountGroups(
-				_user.getCompanyId(), null, QueryUtil.ALL_POS,
-				QueryUtil.ALL_POS, null);
-
-		Assert.assertEquals(0, baseModelSearchResult.getLength());
-		Assert.assertTrue(
-			ListUtil.isEmpty(baseModelSearchResult.getBaseModels()));
-	}
-
-	@Test
 	public void testUpdateAccountGroup() throws Exception {
-		AccountGroup accountGroup = AccountGroupTestUtil.addAccountGroup(
-			_accountGroupLocalService, RandomTestUtil.randomString(),
-			RandomTestUtil.randomString());
+
+		// With resource permission
+
+		User user = UserTestUtil.addUser();
 
 		UserRoleTestUtil.addResourcePermission(
-			ActionKeys.UPDATE, AccountGroup.class.getName(), _user.getUserId());
+			ActionKeys.UPDATE, AccountGroup.class.getName(), user.getUserId());
 
-		_accountGroupService.updateAccountGroup(
-			StringPool.BLANK, accountGroup.getAccountGroupId(),
-			RandomTestUtil.randomString(), accountGroup.getName(),
-			new ServiceContext());
-	}
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				user, PermissionCheckerFactoryUtil.create(user))) {
 
-	@Test(expected = PrincipalException.class)
-	public void testUpdateAccountGroupWithoutPermission() throws Exception {
-		AccountGroup accountGroup = AccountGroupTestUtil.addAccountGroup(
-			_accountGroupLocalService, RandomTestUtil.randomString(),
-			RandomTestUtil.randomString());
+			AccountGroup accountGroup = AccountGroupTestUtil.addAccountGroup(
+				_accountGroupLocalService, RandomTestUtil.randomString(),
+				RandomTestUtil.randomString());
 
-		_accountGroupService.updateAccountGroup(
-			StringPool.BLANK, accountGroup.getAccountGroupId(),
-			RandomTestUtil.randomString(), accountGroup.getName(),
-			new ServiceContext());
+			accountGroup = _accountGroupService.updateAccountGroup(
+				StringPool.BLANK, accountGroup.getAccountGroupId(),
+				RandomTestUtil.randomString(), accountGroup.getName(),
+				new ServiceContext());
+
+			Assert.assertNotNull(accountGroup);
+		}
+
+		// Without resource permission
+
+		user = UserTestUtil.addUser();
+
+		try (ContextUserReplace contextUserReplace = new ContextUserReplace(
+				user, PermissionCheckerFactoryUtil.create(user))) {
+
+			AccountGroup accountGroup = AccountGroupTestUtil.addAccountGroup(
+				_accountGroupLocalService, RandomTestUtil.randomString(),
+				RandomTestUtil.randomString());
+
+			_accountGroupService.updateAccountGroup(
+				StringPool.BLANK, accountGroup.getAccountGroupId(),
+				RandomTestUtil.randomString(), accountGroup.getName(),
+				new ServiceContext());
+
+			Assert.fail();
+		}
+		catch (PrincipalException.MustHavePermission principalException) {
+			Assert.assertNotNull(principalException);
+		}
 	}
 
 	@Inject
@@ -189,7 +289,5 @@ public class AccountGroupServiceTest {
 
 	@Inject
 	private AccountGroupService _accountGroupService;
-
-	private User _user;
 
 }
