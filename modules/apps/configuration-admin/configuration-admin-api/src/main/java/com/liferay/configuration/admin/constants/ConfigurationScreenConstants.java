84bf8c80341bd4faf5892835835396b5dd23c09d
Thiago Buarque <thiago.buarque@liferay.com>
2025-06-11 14:01:46 -0300

    LPD-57054 provide the export and delete URLs for configuration screens that are based on OSGI configurations

---
+++ modules/apps/configuration-admin/configuration-admin-api/src/main/java/com/liferay/configuration/admin/constants/ConfigurationScreenConstants.java
@@ -0,0 +1,19 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.configuration.admin.constants;
+
+/**
+ * @author Thiago Buarque
+ */
+public class ConfigurationScreenConstants {
+
+	public static final String DELETE_CONFIGURATION_URL =
+		"DELETE_CONFIGURATION_URL";
+
+	public static final String EXPORT_CONFIGURATION_URL =
+		"EXPORT_CONFIGURATION_URL";
+
+}
--- modules/apps/configuration-admin/configuration-admin-web/src/main/java/com/liferay/configuration/admin/web/internal/portlet/action/ViewConfigurationScreenMVCRenderCommand.java
+++ modules/apps/configuration-admin/configuration-admin-web/src/main/java/com/liferay/configuration/admin/web/internal/portlet/action/ViewConfigurationScreenMVCRenderCommand.java
@@ -6,23 +6,33 @@
 package com.liferay.configuration.admin.web.internal.portlet.action;
 
 import com.liferay.configuration.admin.constants.ConfigurationAdminPortletKeys;
+import com.liferay.configuration.admin.constants.ConfigurationScreenConstants;
 import com.liferay.configuration.admin.display.ConfigurationScreen;
 import com.liferay.configuration.admin.web.internal.constants.ConfigurationAdminWebKeys;
 import com.liferay.configuration.admin.web.internal.display.ConfigurationEntry;
 import com.liferay.configuration.admin.web.internal.display.ConfigurationScreenConfigurationEntry;
 import com.liferay.configuration.admin.web.internal.display.context.ConfigurationScopeDisplayContext;
 import com.liferay.configuration.admin.web.internal.display.context.ConfigurationScopeDisplayContextFactory;
+import com.liferay.configuration.admin.web.internal.model.ConfigurationModel;
 import com.liferay.configuration.admin.web.internal.util.ConfigurationEntryRetriever;
+import com.liferay.configuration.admin.web.internal.util.ConfigurationModelRetriever;
 import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.portlet.PortletURLUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
+import com.liferay.portal.kernel.portlet.url.builder.ResourceURLBuilder;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 
 import jakarta.portlet.PortletException;
+import jakarta.portlet.PortletURL;
 import jakarta.portlet.RenderRequest;
 import jakarta.portlet.RenderResponse;
+
+import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -88,11 +98,80 @@ public class ViewConfigurationScreenMVCRenderCommand
 		renderRequest.setAttribute(
 			ConfigurationAdminWebKeys.CONFIGURATION_ENTRY, configurationEntry);
 
+		String pid = configurationScreen.getPID();
+
+		if (Validator.isNull(pid)) {
+			return "/view_configuration_screen.jsp";
+		}
+
+		Map<String, ConfigurationModel> configurationModels =
+			_configurationModelRetriever.getConfigurationModels(
+				themeDisplay.getLanguageId(),
+				configurationScopeDisplayContext.getScope(),
+				configurationScopeDisplayContext.getScopePK());
+
+		ConfigurationModel configurationModel = configurationModels.get(pid);
+
+		if (configurationModel != null) {
+			configurationModel = new ConfigurationModel(
+				_configurationModelRetriever.getConfiguration(
+					pid, configurationScopeDisplayContext.getScope(),
+					configurationScopeDisplayContext.getScopePK()),
+				configurationModel);
+
+			renderRequest.setAttribute(
+				ConfigurationScreenConstants.DELETE_CONFIGURATION_URL,
+				_getDeleteConfigurationActionCommand(
+					configurationModel, renderRequest, renderResponse));
+
+			renderRequest.setAttribute(
+				ConfigurationScreenConstants.EXPORT_CONFIGURATION_URL,
+				_getExportConfigurationActionCommand(
+					configurationModel, renderResponse));
+		}
+
 		return "/view_configuration_screen.jsp";
+	}
+
+	private String _getDeleteConfigurationActionCommand(
+		ConfigurationModel configurationModel, RenderRequest renderRequest,
+		RenderResponse renderResponse) {
+
+		PortletURL currentURL = PortletURLUtil.getCurrent(
+			renderRequest, renderResponse);
+
+		return PortletURLBuilder.createActionURL(
+			renderResponse
+		).setActionName(
+			"/configuration_admin/delete_configuration"
+		).setRedirect(
+			currentURL
+		).setParameter(
+			"factoryPid", configurationModel.getFactoryPid()
+		).setParameter(
+			"pid", configurationModel.getID()
+		).buildString();
+	}
+
+	private String _getExportConfigurationActionCommand(
+		ConfigurationModel configurationModel, RenderResponse renderResponse) {
+
+		return ResourceURLBuilder.createResourceURL(
+			renderResponse
+		).setParameter(
+			"factoryPid", configurationModel.getFactoryPid()
+		).setParameter(
+			"pid", configurationModel.getID()
+		).setResourceID(
+			"/configuration_admin/export_configuration"
+		).buildString();
 	}
 
 	@Reference
 	private ConfigurationEntryRetriever _configurationEntryRetriever;
+
+	@Reference(target = "(filter.visibility=*)")
+	private ConfigurationModelRetriever _configurationModelRetriever;
 
 	@Reference
 	private Portal _portal;
