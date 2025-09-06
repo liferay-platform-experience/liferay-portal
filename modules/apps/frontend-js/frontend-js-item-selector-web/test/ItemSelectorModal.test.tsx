4b71e82aaf9650e8c83f559496810b86612095c8
Selena Aungst <selena.aungst@liferay.com>
2025-09-05 17:00:35 -0700

    LPD-46117 Add createItemURL to ItemSelectorModal test.

--- modules/apps/frontend-js/frontend-js-item-selector-web/test/ItemSelectorModal.test.tsx
+++ modules/apps/frontend-js/frontend-js-item-selector-web/test/ItemSelectorModal.test.tsx
@@ -61,10 +61,12 @@ const mockedLoadClientExtensions = loadClientExtensions as jest.Mock;
 const mockedSub = sub as jest.Mock;
 
 const ItemSelectorModalWrapper = ({
+	createItemURL,
 	defaultOpen,
 	onItemsChange,
 	selectedItems,
 }: {
+	createItemURL?: string;
 	defaultOpen: boolean;
 	onItemsChange: (items: TestItem[]) => void;
 	selectedItems: TestItem[];
@@ -78,7 +80,9 @@ const ItemSelectorModalWrapper = ({
 			<ItemSelectorModal<TestItem>
 				{...{
 					apiURL: `${location.origin}/o/headless-delivery/v1.0/test-api-url`,
+					createItemURL,
 					fdsProps: {
+						apiURL: `${location.origin}/o/headless-delivery/v1.0/test-api-url`,
 						id: `itemSelectorModal-test-0001`,
 						pagination: {
 							deltas: [{label: 20}],
