61887840b3bb810937bee6dbd733550412d82459
Pedro Victor Silvestre <pedro.silvestre@liferay.com>
2025-10-27 17:50:39 -0300

    LPD-64256 Fix more tests by unmarking the Sign In utility page as default

--- modules/test/playwright/tests/openid-link/main/openid-link.spec.ts
+++ modules/test/playwright/tests/openid-link/main/openid-link.spec.ts
@@ -14,6 +14,7 @@ import {liferayConfig} from '../../../liferay.config';
 import {OpenIdInstanceSettingsPage} from '../../../pages/portal-settings-authentication-openid-connect-web/OpenIdInstanceSettingsPage';
 import getRandomString from '../../../utils/getRandomString';
 import {performLoginViaApi, performLogout} from '../../../utils/performLogin';
+import {pagesPagesTest} from '../../layout-admin-web/main/fixtures/pagesPagesTest';
 import {utilityPagesPage} from '../../login-web/main/fixtures/utilityPageTest';
 import {openIdConfig} from './config';
 import {openIdSettingsPagesTest} from './fixtures/openIdSettingsPagesTest';
@@ -31,7 +32,8 @@ const test = mergeTests(
 	}),
 	loginTest(),
 	utilityPagesPage,
-	customFieldsPagesTest
+	customFieldsPagesTest,
+	pagesPagesTest
 );
 
 async function setupOpenIdConnection(
@@ -59,12 +61,17 @@ test.afterEach(
 		loginInstanceSettingsPage,
 		openIDInstanceSettingsPage,
 		page,
+		utilityPagesPage,
 	}) => {
 		await page.goto(liferayConfig.environment.baseUrl);
 
 		if (page.getByRole('button', {name: 'Sign In'}).isVisible) {
 			await performLoginViaApi({page, screenName: 'test'});
 		}
+
+		await utilityPagesPage.goto();
+
+		await utilityPagesPage.markAsDefault('Sign In');
 
 		if (providerName) {
 			await openIDInstanceSettingsPage.goto();
@@ -93,6 +100,12 @@ test.afterEach(
 		}
 	}
 );
+
+test.beforeEach(async ({utilityPagesPage}) => {
+	await utilityPagesPage.goto();
+
+	await utilityPagesPage.unmarkAsDefault('Sign In');
+});
 
 test.describe('OpenID connect link', () => {
 	test('is visible on sign-in page when OpenID connection is enabled on NOT an utility page', async ({
