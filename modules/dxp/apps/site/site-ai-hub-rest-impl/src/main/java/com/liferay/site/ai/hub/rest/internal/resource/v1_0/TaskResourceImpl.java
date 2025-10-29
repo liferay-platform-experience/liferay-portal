75bbe8e30711698bcc0850ac4253b41599382249
Feliphe Marinho <feliphemarinho18@gmail.com>
2025-10-29 17:57:31 -0300

    LPD-69638 feat: create postTask method that will start a workflow instance base on a workflow definition with the same name as the type of the task

--- modules/dxp/apps/site/site-ai-hub-rest-impl/src/main/java/com/liferay/site/ai/hub/rest/internal/resource/v1_0/TaskResourceImpl.java
+++ modules/dxp/apps/site/site-ai-hub-rest-impl/src/main/java/com/liferay/site/ai/hub/rest/internal/resource/v1_0/TaskResourceImpl.java
@@ -5,9 +5,21 @@
 
 package com.liferay.site.ai.hub.rest.internal.resource.v1_0;
 
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
+import com.liferay.portal.kernel.service.ServiceContextFactory;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.kernel.workflow.WorkflowInstance;
+import com.liferay.portal.kernel.workflow.WorkflowInstanceManager;
+import com.liferay.site.ai.hub.rest.dto.v1_0.Task;
 import com.liferay.site.ai.hub.rest.resource.v1_0.TaskResource;
 
+import java.io.Serializable;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -18,4 +30,55 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = TaskResource.class
 )
 public class TaskResourceImpl extends BaseTaskResourceImpl {
+
+	@Override
+	public Task postTask(Task task) throws Exception {
+		if (!FeatureFlagManagerUtil.isEnabled(
+				contextCompany.getCompanyId(), "LPD-62272")) {
+
+			throw new UnsupportedOperationException();
+		}
+
+		Map<String, Serializable> workflowContext = _toWorkflowContext(
+			task.getContext());
+
+		WorkflowInstance workflowInstance =
+			_workflowInstanceManager.startWorkflowInstance(
+				contextCompany.getCompanyId(),
+				WorkflowConstants.DEFAULT_GROUP_ID, contextUser.getUserId(),
+				task.getType(), 1, null, workflowContext);
+
+		return new Task() {
+			{
+				setExternalReferenceCode(
+					() -> String.valueOf(
+						workflowInstance.getWorkflowInstanceId()));
+				setType(task::getType);
+			}
+		};
+	}
+
+	private Map<String, Serializable> _toWorkflowContext(Map<String, ?> context)
+		throws Exception {
+
+		Map<String, Serializable> workflowContext = new HashMap<>();
+
+		for (Map.Entry<String, ?> entry : context.entrySet()) {
+			Object value = entry.getValue();
+
+			if (value instanceof Serializable) {
+				workflowContext.put(entry.getKey(), (Serializable)value);
+			}
+		}
+
+		workflowContext.put(
+			WorkflowConstants.CONTEXT_SERVICE_CONTEXT,
+			ServiceContextFactory.getInstance(contextHttpServletRequest));
+
+		return workflowContext;
+	}
+
+	@Reference
+	private WorkflowInstanceManager _workflowInstanceManager;
+
 }
