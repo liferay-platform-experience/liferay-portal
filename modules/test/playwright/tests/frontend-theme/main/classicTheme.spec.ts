00a045e57dc3effe8677d579ce6dee6ff2a384c4
Mario Leandro <mario.leandro@liferay.com>
2025-11-03 10:44:16 -0300

    LPD-70289 Rename variable to avoid abbreviations

--- modules/test/playwright/tests/frontend-theme/main/classicTheme.spec.ts
+++ modules/test/playwright/tests/frontend-theme/main/classicTheme.spec.ts
@@ -25,7 +25,7 @@ export const test = mergeTests(
 
 const PAGE_NAME = 'Classic Theme Page';
 const PORTLET_NAME = 'Clay Sample';
-const CUSTOM_BG_COLOR = 'rgb(66, 244, 197)'
+const CUSTOM_BACKGROUND_COLOR = 'rgb(66, 244, 197)'
 
 const goToPortletOptions = async ({page}: {page: Page}) => {
 	await page
@@ -91,7 +91,7 @@ test(
 
 			await page
 				.getByRole('textbox', {exact: true, name: 'CSS'})
-				.fill(`body, #wrapper{background-color: ${CUSTOM_BG_COLOR};}`);
+				.fill(`body, #wrapper{background-color: ${CUSTOM_BACKGROUND_COLOR};}`);
 
 			await pageConfigurationPage.save();
 		});
@@ -101,12 +101,12 @@ test(
 
 			await expect(page.locator('body')).toHaveCSS(
 				'background-color',
-				CUSTOM_BG_COLOR
+				CUSTOM_BACKGROUND_COLOR
 			);
 
 			await expect(page.locator('#wrapper')).toHaveCSS(
 				'background-color',
-				CUSTOM_BG_COLOR
+				CUSTOM_BACKGROUND_COLOR
 			);
 		});
 
