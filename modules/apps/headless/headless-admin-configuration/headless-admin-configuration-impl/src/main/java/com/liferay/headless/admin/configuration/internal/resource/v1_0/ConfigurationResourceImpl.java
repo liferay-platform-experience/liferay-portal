f8e1f2feb5ebd67f4e89301549f395a44c9dbec7
Thiago Buarque <thiago.buarque@liferay.com>
2025-11-04 10:49:13 -0300

    LPD-69794 buildREST

---
+++ modules/apps/headless/headless-admin-configuration/headless-admin-configuration-impl/src/main/java/com/liferay/headless/admin/configuration/internal/resource/v1_0/ConfigurationResourceImpl.java
@@ -0,0 +1,21 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.headless.admin.configuration.internal.resource.v1_0;
+
+import com.liferay.headless.admin.configuration.resource.v1_0.ConfigurationResource;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ServiceScope;
+
+/**
+ * @author Thiago Buarque
+ */
+@Component(
+	properties = "OSGI-INF/liferay/rest/v1_0/configuration.properties",
+	scope = ServiceScope.PROTOTYPE, service = ConfigurationResource.class
+)
+public class ConfigurationResourceImpl extends BaseConfigurationResourceImpl {
+}
