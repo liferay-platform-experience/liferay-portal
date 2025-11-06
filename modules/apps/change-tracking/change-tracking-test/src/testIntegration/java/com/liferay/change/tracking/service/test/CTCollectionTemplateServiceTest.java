e522a219774d5bd6e5bd79309fc1fbeedc91867f
brookedalton <brooke.dalton@liferay.com>
2025-11-06 13:20:30 -0800

    LPD-70341 Delete data in teardown

--- modules/apps/change-tracking/change-tracking-test/src/testIntegration/java/com/liferay/change/tracking/service/test/CTCollectionTemplateServiceTest.java
+++ modules/apps/change-tracking/change-tracking-test/src/testIntegration/java/com/liferay/change/tracking/service/test/CTCollectionTemplateServiceTest.java
@@ -7,12 +7,13 @@ package com.liferay.change.tracking.service.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.change.tracking.model.CTCollectionTemplate;
+import com.liferay.change.tracking.service.CTCollectionTemplateLocalService;
 import com.liferay.change.tracking.service.CTCollectionTemplateService;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
-import com.liferay.portal.kernel.test.rule.DataGuard;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
@@ -21,6 +22,7 @@ import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
 import java.util.Collections;
 import java.util.List;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
@@ -31,7 +33,6 @@ import org.junit.runner.RunWith;
 /**
  * @author Noor Najjar
  */
-@DataGuard(scope = DataGuard.Scope.METHOD)
 @RunWith(Arquillian.class)
 public class CTCollectionTemplateServiceTest {
 
@@ -46,19 +47,26 @@ public class CTCollectionTemplateServiceTest {
 	public void setUp() throws Exception {
 		_name = RandomTestUtil.randomString();
 
-		_ctCollectionTemplateService.addCTCollectionTemplate(
-			_name, RandomTestUtil.randomString(),
-			JSONUtil.put(
-				"description", RandomTestUtil.randomString()
-			).put(
-				"name", RandomTestUtil.randomString()
-			).put(
-				"publicationsUserRoleUserIds", Collections.emptyList()
-			).put(
-				"roleValues", Collections.emptyList()
-			).put(
-				"userIds", new long[] {RandomTestUtil.randomLong()}
-			).toString());
+		_ctCollectionTemplate =
+			_ctCollectionTemplateService.addCTCollectionTemplate(
+				_name, RandomTestUtil.randomString(),
+				JSONUtil.put(
+					"description", RandomTestUtil.randomString()
+				).put(
+					"name", RandomTestUtil.randomString()
+				).put(
+					"publicationsUserRoleUserIds", Collections.emptyList()
+				).put(
+					"roleValues", Collections.emptyList()
+				).put(
+					"userIds", new long[] {RandomTestUtil.randomLong()}
+				).toString());
+	}
+
+	@After
+	public void tearDown() throws PortalException {
+		_ctCollectionTemplateLocalService.deleteCTCollectionTemplate(
+			_ctCollectionTemplate);
 	}
 
 	@Test
@@ -95,8 +103,13 @@ public class CTCollectionTemplateServiceTest {
 	}
 
 	@Inject
+	private static CTCollectionTemplateLocalService
+		_ctCollectionTemplateLocalService;
+
+	@Inject
 	private static CTCollectionTemplateService _ctCollectionTemplateService;
 
+	private CTCollectionTemplate _ctCollectionTemplate;
 	private String _name;
 
 }
