6284eb9ed0b1a699c034f07092f0fb1c8aef8632
Thiago Buarque <thiago.buarque@liferay.com>
2025-11-07 13:46:40 -0300

    LPD-70703 expose issue with test scenario

---
+++ modules/apps/configuration-admin/configuration-admin-api/src/test/java/com/liferay/configuration/admin/util/ConfigurationPIDUtilTest.java
@@ -0,0 +1,41 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.configuration.admin.util;
+
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author Thiago Buarque
+ */
+public class ConfigurationPIDUtilTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@Test
+	public void testGetRawPid() {
+		String expectedRawPid =
+			"com.liferay.scoped.configuration.SampleConfiguration";
+
+		Assert.assertEquals(
+			expectedRawPid,
+			ConfigurationPIDUtil.getRawPid(
+				"com.liferay.scoped.configuration.SampleConfiguration~123"));
+		Assert.assertEquals(
+			expectedRawPid,
+			ConfigurationPIDUtil.getRawPid(
+				"com.liferay.scoped.configuration.SampleConfiguration.scoped" +
+					"~123"));
+	}
+
+}
