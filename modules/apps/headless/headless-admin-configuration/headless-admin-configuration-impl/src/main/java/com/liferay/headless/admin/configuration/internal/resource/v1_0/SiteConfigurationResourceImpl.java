ee00ffd97f3e3c05551245c8f9e316e2f18996ce
Thiago Buarque <thiago.buarque@liferay.com>
2025-11-12 13:52:37 -0300

    LPD-70602 allow exporting site configurations

--- modules/apps/headless/headless-admin-configuration/headless-admin-configuration-impl/src/main/java/com/liferay/headless/admin/configuration/internal/resource/v1_0/SiteConfigurationResourceImpl.java
+++ modules/apps/headless/headless-admin-configuration/headless-admin-configuration-impl/src/main/java/com/liferay/headless/admin/configuration/internal/resource/v1_0/SiteConfigurationResourceImpl.java
@@ -5,9 +5,37 @@
 
 package com.liferay.headless.admin.configuration.internal.resource.v1_0;
 
+import com.liferay.configuration.admin.exportimport.ConfigurationExportImportProcessor;
+import com.liferay.configuration.admin.util.ConfigurationFilterStringUtil;
+import com.liferay.headless.admin.configuration.dto.v1_0.SiteConfiguration;
+import com.liferay.headless.admin.configuration.internal.util.ConfigurationUtil;
 import com.liferay.headless.admin.configuration.resource.v1_0.SiteConfigurationResource;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.settings.SettingsLocatorHelper;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.vulcan.pagination.Page;
 
+import jakarta.ws.rs.BadRequestException;
+import jakarta.ws.rs.NotAuthorizedException;
+import jakarta.ws.rs.NotFoundException;
+import jakarta.ws.rs.core.Response;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -19,4 +47,137 @@ import org.osgi.service.component.annotations.ServiceScope;
 )
 public class SiteConfigurationResourceImpl
 	extends BaseSiteConfigurationResourceImpl {
+
+	@Override
+	public SiteConfiguration getSiteSiteConfiguration(
+			String siteExternalReferenceCode,
+			String siteConfigurationExternalReferenceCode)
+		throws Exception {
+
+		if (!FeatureFlagManagerUtil.isEnabled(
+				contextCompany.getCompanyId(), "LPD-65399")) {
+
+			throw new UnsupportedOperationException();
+		}
+
+		Group group = _groupLocalService.getGroupByExternalReferenceCode(
+			siteExternalReferenceCode, contextCompany.getCompanyId());
+
+		_checkPermission(group.getGroupId());
+
+		Configuration[] configurations = _configurationAdmin.listConfigurations(
+			ConfigurationFilterStringUtil.getGroupScopedFilterString(
+				String.valueOf(group.getGroupId()),
+				siteConfigurationExternalReferenceCode,
+				siteExternalReferenceCode));
+
+		if (ArrayUtil.isEmpty(configurations)) {
+			throw new NotFoundException(
+				"Unable to find site configuration with external reference " +
+					"code " + siteConfigurationExternalReferenceCode);
+		}
+
+		if (configurations.length > 1) {
+			List<String> pids = new ArrayList<>();
+
+			for (Configuration configuration : configurations) {
+				pids.add(configuration.getPid());
+			}
+
+			throw new BadRequestException(
+				StringBundler.concat(
+					siteConfigurationExternalReferenceCode,
+					" is a factory configuration. Specify one of the entries ",
+					"pid ",
+					ListUtil.toString(pids, StringPool.BLANK, StringPool.COMMA),
+					"."));
+		}
+
+		return _toSiteConfiguration(configurations[0]);
+	}
+
+	@Override
+	public Page<SiteConfiguration> getSiteSiteConfigurationsPage(
+			String siteExternalReferenceCode)
+		throws Exception {
+
+		if (!FeatureFlagManagerUtil.isEnabled(
+				contextCompany.getCompanyId(), "LPD-65399")) {
+
+			throw new UnsupportedOperationException();
+		}
+
+		Group group = _groupLocalService.getGroupByExternalReferenceCode(
+			siteExternalReferenceCode, contextCompany.getCompanyId());
+
+		_checkPermission(group.getGroupId());
+
+		Configuration[] configurations = _configurationAdmin.listConfigurations(
+			ConfigurationFilterStringUtil.getGroupScopedFilterString(
+				String.valueOf(group.getGroupId()), siteExternalReferenceCode));
+
+		if (ArrayUtil.isEmpty(configurations)) {
+			return Page.of(Collections.emptyList());
+		}
+
+		List<SiteConfiguration> siteConfigurations = new ArrayList<>();
+
+		for (Configuration configuration : configurations) {
+			SiteConfiguration siteConfiguration = _toSiteConfiguration(
+				configuration);
+
+			if (siteConfiguration == null) {
+				continue;
+			}
+
+			siteConfigurations.add(siteConfiguration);
+		}
+
+		return Page.of(siteConfigurations);
+	}
+
+	private void _checkPermission(long groupId) {
+		PermissionChecker permissionChecker =
+			PermissionThreadLocal.getPermissionChecker();
+
+		if (!permissionChecker.isCompanyAdmin() &&
+			!permissionChecker.isGroupAdmin(groupId) &&
+			!permissionChecker.isOmniadmin()) {
+
+			throw new NotAuthorizedException(Response.Status.UNAUTHORIZED);
+		}
+	}
+
+	private SiteConfiguration _toSiteConfiguration(Configuration configuration)
+		throws Exception {
+
+		Map<String, Object> properties = ConfigurationUtil.getProperties(
+			configuration, _configurationExportImportProcessor,
+			_settingsLocatorHelper);
+
+		if (properties.isEmpty()) {
+			return null;
+		}
+
+		SiteConfiguration siteConfiguration = new SiteConfiguration();
+
+		siteConfiguration.setExternalReferenceCode(configuration::getPid);
+		siteConfiguration.setProperties(() -> properties);
+
+		return siteConfiguration;
+	}
+
+	@Reference
+	private ConfigurationAdmin _configurationAdmin;
+
+	@Reference
+	private ConfigurationExportImportProcessor
+		_configurationExportImportProcessor;
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+
+	@Reference
+	private SettingsLocatorHelper _settingsLocatorHelper;
+
 }
