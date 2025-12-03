af63215c0b28248efbe8bd7350c9a59d2332e94e
Marko Čikoš <marko.cikos@liferay.com>
2025-12-03 13:07:21 +0100

    LPD-70790 Move `isReducedMotion` to `accessibility-settings-state-web`, to prevent circular dependency between `frontend-js-web` and `frontend-js-state-web`

--- modules/apps/accessibility/accessibility-settings-state-web/src/main/resources/META-INF/resources/isReducedMotion.ts
+++ modules/apps/accessibility/accessibility-settings-state-web/src/main/resources/META-INF/resources/isReducedMotion.ts
@@ -3,11 +3,9 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {
-	CONSTANTS,
-	accessibilityMenuAtom,
-} from '@liferay/accessibility-settings-state-web';
 import {State} from '@liferay/frontend-js-state-web';
+
+import {CONSTANTS, accessibilityMenuAtom} from '.';
 
 function isReducedMotion() {
 	const accessibilityMenu = State.read(accessibilityMenuAtom);
--- modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/main/index.js
+++ modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/main/index.js
@@ -90,7 +90,6 @@ export {
 export {default as AOP} from './aop/AOP.es';
 export {default as buildFragment} from './util/build_fragment';
 export {default as printPage} from './util/print_page';
-export {isReducedMotion} from './util/reducedMotion';
 export {default as dateUtils} from './utils/date_time/index';
 export {getObjectValueFromPath} from './utils/object/getObjectValueFromPath';
 export {default as pkceChallenge} from './utils/pkceChallenge';
