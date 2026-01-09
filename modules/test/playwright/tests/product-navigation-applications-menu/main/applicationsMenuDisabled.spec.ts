e3c53c049bd8bb50cb6c89a28da48de305e38132
Gabriel Lima <gabriel.lima@liferay.com>
2026-01-09 09:35:01 -0300

    LPD-66980 Assert Product Menu replaces the Applications Menu when it is disabled in a virtual instance

--- modules/test/playwright/tests/product-navigation-applications-menu/main/applicationsMenuDisabled.spec.ts
+++ modules/test/playwright/tests/product-navigation-applications-menu/main/applicationsMenuDisabled.spec.ts
@@ -3,37 +3,38 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {expect, mergeTests} from '@playwright/test';
+import {Page, expect, mergeTests} from '@playwright/test';
 
 import {instanceSettingsPagesTest} from '../../../fixtures/instanceSettingsPagesTest';
 import {isolatedSiteTest} from '../../../fixtures/isolatedSiteTest';
 import {loginTest} from '../../../fixtures/loginTest';
 import {productMenuPageTest} from '../../../fixtures/productMenuPageTest';
+import {virtualInstancesPagesTest} from '../../../fixtures/virtualInstancesPagesTest';
 import {InstanceSettingsPage} from '../../../pages/configuration-admin-web/InstanceSettingsPage';
+import {ApplicationsMenuPage} from '../../../pages/product-navigation-applications-menu/ApplicationsMenuPage';
+import {ProductMenuPage} from '../../../pages/product-navigation-control-menu-web/ProductMenuPage';
 import {clickAndExpectToBeVisible} from '../../../utils/clickAndExpectToBeVisible';
+import performLogin from '../../../utils/performLogin';
 
-async function goToApplicationsMenuSetting(
-	instanceSettingsPage: InstanceSettingsPage
+const MENU_TEST_PAGES = [
+	{category: 'Sites', name: 'Sites', panel: 'Control Panel'},
+	{category: 'Order Management', name: 'Orders', panel: 'Commerce'},
+	{
+		category: 'Content',
+		name: 'Asset Libraries',
+		panel: 'Applications',
+	},
+];
+
+const VIRTUAL_INSTANCE_NAME = 'www.able.com';
+
+async function disableApplicationsMenu(
+	instanceSettingsPage: InstanceSettingsPage,
+	forceReload: boolean
 ) {
-	await instanceSettingsPage.goto();
+	await goToApplicationsMenuSetting(instanceSettingsPage, forceReload);
 
-	await instanceSettingsPage.goToInstanceSetting(
-		'Navigation',
-		'Applications Menu'
-	);
-}
-
-const test = mergeTests(
-	instanceSettingsPagesTest,
-	isolatedSiteTest,
-	productMenuPageTest,
-	loginTest()
-);
-
-test.beforeEach(async ({instanceSettingsPage, page, productMenuPage}) => {
-	await goToApplicationsMenuSetting(instanceSettingsPage);
-
-	const checkbox = page.getByRole('checkbox', {
+	const checkbox = instanceSettingsPage.page.getByRole('checkbox', {
 		name: 'Enable Applications Menu',
 	});
 
@@ -42,82 +43,199 @@ test.beforeEach(async ({instanceSettingsPage, page, productMenuPage}) => {
 	}
 
 	await instanceSettingsPage.saveAndWaitForAlert();
+}
 
-	await productMenuPage.openProductMenuIfClosed();
-});
+async function goToApplicationsMenuSetting(
+	instanceSettingsPage: InstanceSettingsPage,
+	forceReload: boolean
+) {
+	await instanceSettingsPage.goto(forceReload);
 
-test.afterEach(async ({instanceSettingsPage}) => {
-	await goToApplicationsMenuSetting(instanceSettingsPage);
+	await instanceSettingsPage.goToInstanceSetting(
+		'Navigation',
+		'Applications Menu',
+		forceReload
+	);
+}
+
+async function resetApplicationsMenu(
+	instanceSettingsPage: InstanceSettingsPage,
+	forceReload: boolean
+) {
+	await goToApplicationsMenuSetting(instanceSettingsPage, forceReload);
 
 	await instanceSettingsPage.resetInstanceSetting();
-});
+}
 
-test(
-	'The Product Menu replaces the Applications Menu when it is disabled',
-	{tag: '@LPD-66980'},
-	async ({page, productMenuPage, site}) => {
-		const siteName = page.locator('.site-name');
+const test = mergeTests(
+	instanceSettingsPagesTest,
+	isolatedSiteTest,
+	productMenuPageTest,
+	virtualInstancesPagesTest,
+	loginTest()
+);
 
-		await test.step('It allows a user to choose a different site', async () => {
-			await expect(siteName).toHaveText('Liferay DXP');
+test.describe('Disabled Applications Menu - Default Instance', () => {
+	test.beforeEach(async ({instanceSettingsPage, productMenuPage}) => {
+		await disableApplicationsMenu(instanceSettingsPage, true);
 
-			const frame = page.frameLocator('iframe[title="Select Site"]');
+		await productMenuPage.openProductMenuIfClosed();
+	});
 
-			const allSitesTab = frame.getByRole('link', {name: 'All Sites'});
+	test.afterEach(async ({instanceSettingsPage}) => {
+		await resetApplicationsMenu(instanceSettingsPage, true);
+	});
 
-			await clickAndExpectToBeVisible({
-				target: allSitesTab,
-				trigger: page.getByRole('button', {name: 'Go to Other Site'}),
-			});
+	test(
+		'The Product Menu replaces the Applications Menu when it is disabled',
+		{tag: '@LPD-66980'},
+		async ({page, productMenuPage, site}) => {
+			const siteName = page.locator('.site-name');
 
-			const siteLink = frame.getByRole('link', {
-				exact: true,
-				name: site.name,
-			});
+			await test.step('It allows a user to choose a different site', async () => {
+				await expect(siteName).toHaveText(/Liferay DXP|Choose a Site/);
 
-			await clickAndExpectToBeVisible({
-				autoClick: true,
-				target: siteLink,
-				trigger: allSitesTab,
-			});
+				const frame = page.frameLocator('iframe[title="Select Site"]');
 
-			await expect(siteName).toHaveText(site.name);
-		});
+				const allSitesTab = frame.getByRole('link', {
+					name: 'All Sites',
+				});
 
-		await test.step('Collapse Site Administration Panel', async () => {
-			await clickAndExpectToBeVisible({
-				target: page.getByRole('button', {
+				await clickAndExpectToBeVisible({
+					target: allSitesTab,
+					trigger: page.getByRole('button', {
+						name: 'Go to Other Site',
+					}),
+				});
+
+				const siteLink = frame.getByRole('link', {
 					exact: true,
-					expanded: false,
 					name: site.name,
-				}),
-				trigger: siteName,
-			});
-		});
+				});
 
-		const testPages = [
-			{
+				await clickAndExpectToBeVisible({
+					autoClick: true,
+					target: siteLink,
+					trigger: allSitesTab,
+				});
+
+				await expect(siteName).toHaveText(site.name);
+			});
+
+			await test.step('Collapse Site Administration Panel', async () => {
+				await clickAndExpectToBeVisible({
+					target: page.getByRole('button', {
+						exact: true,
+						expanded: false,
+						name: site.name,
+					}),
+					trigger: siteName,
+				});
+			});
+
+			const menuTestPages = MENU_TEST_PAGES.concat({
 				category: 'Site Builder',
 				name: 'Pages',
 				panel: site.name,
-			},
-			{
-				category: 'Content',
-				name: 'Asset Libraries',
-				panel: 'Applications',
-			},
-			{category: 'Order Management', name: 'Orders', panel: 'Commerce'},
-			{category: 'Sites', name: 'Sites', panel: 'Control Panel'},
-		];
-
-		for (const {category, name, panel} of testPages) {
-			await test.step(`Navigate to ${panel} > ${category} > ${name} via Product Menu`, async () => {
-				await productMenuPage.goToPortlet(panel, category, name);
-
-				await expect(
-					page.getByRole('heading', {exact: true, name})
-				).toBeVisible();
 			});
+
+			for (const {category, name, panel} of menuTestPages) {
+				await test.step(`Navigate to ${panel} > ${category} > ${name} via Product Menu`, async () => {
+					await productMenuPage.goToPortlet(panel, category, name);
+
+					await expect(
+						page.getByRole('heading', {exact: true, name})
+					).toBeVisible();
+				});
+			}
 		}
-	}
-);
+	);
+});
+
+test.describe('Disabled Applications Menu - Virtual Instance', () => {
+	let virtualInstancePage: Page;
+	let virtualInstanceInstanceSettingsPage: InstanceSettingsPage;
+
+	test.slow();
+
+	test.beforeEach(
+		async ({browser, instanceSettingsPage, virtualInstancesPage}) => {
+			await virtualInstancesPage.addNewVirtualInstance(
+				VIRTUAL_INSTANCE_NAME
+			);
+
+			virtualInstancePage = await browser.newPage({
+				baseURL: `http://${VIRTUAL_INSTANCE_NAME}:8080`,
+			});
+
+			virtualInstanceInstanceSettingsPage = new InstanceSettingsPage(
+				virtualInstancePage
+			);
+
+			await performLogin(
+				virtualInstancePage,
+				'test',
+				'',
+				`@${VIRTUAL_INSTANCE_NAME}.com`
+			);
+
+			await disableApplicationsMenu(instanceSettingsPage, true);
+		}
+	);
+
+	test.afterEach(async ({instanceSettingsPage, virtualInstancesPage}) => {
+		await resetApplicationsMenu(virtualInstanceInstanceSettingsPage, false);
+		await resetApplicationsMenu(instanceSettingsPage, true);
+
+		await virtualInstancePage?.close();
+
+		await virtualInstancesPage.deleteVirtualInstance(VIRTUAL_INSTANCE_NAME);
+	});
+
+	test(
+		'The Product Menu replaces the Applications Menu when it is disabled in a Virtual Instance',
+		{tag: '@LPD-66980'},
+		async () => {
+			const virtualInstanceProductMenuPage = new ProductMenuPage(
+				virtualInstancePage
+			);
+
+			await test.step('Disable Applications Menu in virtual instance', async () => {
+				const virtualInstanceApplicationsMenuPage =
+					new ApplicationsMenuPage(virtualInstancePage);
+
+				expect(
+					await virtualInstanceApplicationsMenuPage.isApplicationsMenuButtonVisible()
+				).toBeTruthy();
+
+				await disableApplicationsMenu(
+					virtualInstanceInstanceSettingsPage,
+					false
+				);
+
+				expect(
+					await virtualInstanceApplicationsMenuPage.isApplicationsMenuButtonVisible()
+				).toBeFalsy();
+
+				await virtualInstanceProductMenuPage.openProductMenuIfClosed();
+			});
+
+			for (const {category, name, panel} of MENU_TEST_PAGES) {
+				await test.step(`Navigate to ${panel} > ${category} > ${name} via Product Menu`, async () => {
+					await virtualInstanceProductMenuPage.goToPortlet(
+						panel,
+						category,
+						name
+					);
+
+					await expect(
+						virtualInstancePage.getByRole('heading', {
+							exact: true,
+							name,
+						})
+					).toBeVisible();
+				});
+			}
+		}
+	);
+});
