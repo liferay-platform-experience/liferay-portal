48429382c757682167a45bc10b0db2c14b21c606
Rafael Uen <rafael.uen@liferay.com>
2026-01-12 17:22:15 -0300

    LPD-73265 Add featureFlag to test

--- modules/test/playwright/tests/object-web/main/objectEntries.spec.ts
+++ modules/test/playwright/tests/object-web/main/objectEntries.spec.ts
@@ -80,6 +80,7 @@ const assigneeTest = test;
 const cmsTest = mergeTests(
 	test,
 	featureFlagsTest({
+		'LPD-11235': {enabled: true},
 		'LPD-17564': {enabled: true},
 		'LPD-34594': {enabled: true},
 	})
