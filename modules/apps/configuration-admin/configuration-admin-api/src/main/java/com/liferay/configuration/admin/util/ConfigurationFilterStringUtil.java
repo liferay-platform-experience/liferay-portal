205e57af86a2d1c49b740b8341cd90a8fdc2483d
Thiago Buarque <thiago.buarque@liferay.com>
2026-01-12 11:53:47 -0300

    LPD-75854 add getScopedFilterString to handle cases where the client code needs to condition the search based on the scope it gets

--- modules/apps/configuration-admin/configuration-admin-api/src/main/java/com/liferay/configuration/admin/util/ConfigurationFilterStringUtil.java
+++ modules/apps/configuration-admin/configuration-admin-api/src/main/java/com/liferay/configuration/admin/util/ConfigurationFilterStringUtil.java
@@ -121,6 +121,25 @@ public class ConfigurationFilterStringUtil {
 			StringPool.CLOSE_PARENTHESIS);
 	}
 
+	public static String getScopedFilterString(
+		String pid, ExtendedObjectClassDefinition.Scope scope,
+		Serializable scopePK) {
+
+		if (scope.equals(ExtendedObjectClassDefinition.Scope.COMPANY)) {
+			return getCompanyScopedFilterString(scopePK, pid, null);
+		}
+		else if (scope.equals(ExtendedObjectClassDefinition.Scope.GROUP)) {
+			return getGroupScopedFilterString(scopePK, pid, null);
+		}
+		else if (scope.equals(
+					ExtendedObjectClassDefinition.Scope.PORTLET_INSTANCE)) {
+
+			return getPortletScopedFilterString(pid, scopePK);
+		}
+
+		return getSystemScopedFilterString(pid);
+	}
+
 	public static String getSystemScopedFilterString() {
 		return StringBundler.concat(
 			"(&(|(!(",
