78cd02f711dea7bc137f8662412bcd57fc4af25b
Mario Leandro <mario.leandro@liferay.com>
2026-01-12 15:16:39 -0300

    LPD-75855 Fix FolderTree at journal-web module to match new Clay's version v3.156.6

--- modules/apps/journal/journal-web/src/main/resources/META-INF/resources/js/SelectFolder.js
+++ modules/apps/journal/journal-web/src/main/resources/META-INF/resources/js/SelectFolder.js
@@ -80,8 +80,16 @@ function FolderTree({filterQuery, handleSelectionChange, items: initialItems}) {
 		return nodeByName(items, filterQuery);
 	}, [items, filterQuery]);
 
-	const onClick = (event, item) => {
+	const onClick = (event, item, expand) => {
 		event.preventDefault();
+
+		const isExpander = event.target.closest('.component-expander');
+
+		if (isExpander) {
+			expand.toggle(item.id);
+
+			return;
+		}
 
 		handleSelectionChange(item);
 	};
@@ -100,10 +108,10 @@ function FolderTree({filterQuery, handleSelectionChange, items: initialItems}) {
 			onItemsChange={setItems}
 			showExpanderOnHover={false}
 		>
-			{(item) => (
+			{(item, _, expand) => (
 				<ClayTreeView.Item>
 					<ClayTreeView.ItemStack
-						onClick={(event) => onClick(event, item)}
+						onClick={(event) => onClick(event, item, expand)}
 						onKeyUp={(event) => onKeyUp(event, item)}
 					>
 						<ClayIcon symbol="folder" />
