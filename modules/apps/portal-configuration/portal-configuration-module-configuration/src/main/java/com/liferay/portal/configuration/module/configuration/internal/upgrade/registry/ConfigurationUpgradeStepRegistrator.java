672e502d39e39409022b4f620cf73e8d67fd99c9
Thiago Buarque <thiago.buarque@liferay.com>
2026-01-15 14:07:30 -0300

    LPD-75854 add upgrade process

---
+++ modules/apps/portal-configuration/portal-configuration-module-configuration/src/main/java/com/liferay/portal/configuration/module/configuration/internal/upgrade/registry/ConfigurationUpgradeStepRegistrator.java
@@ -0,0 +1,34 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.configuration.module.configuration.internal.upgrade.registry;
+
+import com.liferay.portal.configuration.module.configuration.internal.upgrade.v1_0_0.ConfigurationUpgradeProcess;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Thiago Buarque
+ */
+@Component(service = UpgradeStepRegistrator.class)
+public class ConfigurationUpgradeStepRegistrator
+	implements UpgradeStepRegistrator {
+
+	@Override
+	public void register(Registry registry) {
+		registry.registerInitialization();
+
+		registry.register(
+			"0.0.1", "1.0.0",
+			new ConfigurationUpgradeProcess(_groupLocalService));
+	}
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+
+}
