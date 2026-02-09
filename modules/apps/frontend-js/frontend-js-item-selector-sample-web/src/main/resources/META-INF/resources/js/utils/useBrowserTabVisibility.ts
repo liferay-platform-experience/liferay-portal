62aeb9b5e5d242c168c835667977c5e2b1270131
Juanjo Fernandez <juanjo.fernandez@liferay.com>
2026-02-09 08:58:20 +0100

    LPD-66120 - Feat. Add custom hook to check for document/tab visibility.

---
+++ modules/apps/frontend-js/frontend-js-item-selector-sample-web/src/main/resources/META-INF/resources/js/utils/useBrowserTabVisibility.ts
@@ -0,0 +1,31 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {useSyncExternalStore} from 'react';
+
+const useVisibilityChangeEventSubscribe = (callback: () => void) => {
+	document.addEventListener('visibilitychange', callback);
+
+	return () => {
+		document.removeEventListener('visibilitychange', callback);
+	};
+};
+
+const getVisibilityChangeEventSnapshot = () => {
+	return document.visibilityState;
+};
+
+export function useBrowserTabVisibility() {
+	const visibilityState = useSyncExternalStore(
+
+		// eslint-disable-next-line react-compiler/react-compiler
+		useVisibilityChangeEventSubscribe,
+		getVisibilityChangeEventSnapshot
+	);
+
+	return visibilityState === 'visible';
+}
+
+export default useBrowserTabVisibility;
