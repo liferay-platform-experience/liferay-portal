298ea4b8135b218b9a8d3ae212a430363df28aab
Ambrin Chaudhary <ambrin.chaudhary@liferay.com>
2026-02-17 13:25:48 +0100

    LPD-67215 Item Selector should have the same config than in CKEditor plugin (HeadlessItemSelector)

--- modules/apps/fragment/fragment-impl/src/main/resources/META-INF/resources/js/api/openCMSItemSelectorModal.ts
+++ modules/apps/fragment/fragment-impl/src/main/resources/META-INF/resources/js/api/openCMSItemSelectorModal.ts
@@ -5,75 +5,101 @@
 
 import {openItemSelectorModal} from '@liferay/frontend-js-item-selector-web';
 
+const CMS_FILE_ITEM_SELECTOR_CONFIG = {
+	apiURL: `${location.origin}/o/search/v1.0/search?${[
+		'emptySearch=true',
+		'nestedFields=description,embedded,file.thumbnailURL',
+		"filter=(cmsKind eq 'object') and (cmsSection eq 'files') and (status in (0, 2, 3))",
+	].join('&')}`,
+	items: [],
+	locator: {
+		id: 'embedded.id',
+		label: 'embedded.title',
+		value: 'embedded.id',
+	},
+	multiSelect: false,
+};
+
+const FDS_PROPS = {
+	filters: [
+		{
+			apiURL: '/o/headless-asset-library/v1.0/asset-libraries',
+			entityFieldType: 'collection',
+			id: 'groupIds',
+			itemKey: 'siteId',
+			itemLabel: 'name',
+			label: Liferay.Language.get('space'),
+			multiple: true,
+			type: 'selection',
+		},
+	],
+	id: '',
+	pagination: {
+		deltas: [{label: 20}, {label: 40}, {label: 60}],
+		initialDelta: 20,
+	},
+	views: [
+		{
+			contentRenderer: 'cards',
+			label: Liferay.Language.get('cards'),
+			name: 'cards',
+			schema: {
+				description: 'description',
+				symbol: '',
+				title: 'title',
+			},
+
+			setItemComponentProps: ({
+				item,
+				props,
+			}: {
+				item: {
+					embedded:
+						| {coverImage: {link: {href: string}}}
+						| {file: {thumbnailURL: string}};
+				};
+				props: object;
+			}) => {
+				const stickerProps = {
+					stickerProps: {
+						className: 'file-icon-color-5',
+						displayType: 'unstyled',
+					},
+				};
+
+				if ('file' in item.embedded) {
+					return {
+						...props,
+						imgProps: {src: item.embedded.file.thumbnailURL},
+						...stickerProps,
+					};
+				}
+
+				return {
+					...props,
+					...stickerProps,
+				};
+			},
+
+			thumbnail: 'cards2',
+		},
+	],
+};
+
+function getRandomId(): string {
+	return Math.random().toString(36).substring(2, 9);
+}
+
 export const openCMSItemSelectorModal = function ({
 	onSelect,
 }: {
 	onSelect: () => void;
 }) {
-	const ROOT_URL = `${window.location.origin}${Liferay.ThemeDisplay.getPathContext()}/o/search/v1.0/search`;
-
-	const BASE_SEARCH_PARAMS = {
-		currentURL: '/web/cms/files',
-		emptySearch: 'true',
-		nestedFields: 'description,embedded,file.thumbnailURL',
-	};
-
-	const CMS_ROOT_FILES_URL = `${ROOT_URL}?${new URLSearchParams({
-		...BASE_SEARCH_PARAMS,
-		filter: "cmsRoot eq true and cmsSection eq 'files' and status in (0, 2, 3)",
-	}).toString()}`;
-
 	openItemSelectorModal({
-		apiURL: CMS_ROOT_FILES_URL,
+		...CMS_FILE_ITEM_SELECTOR_CONFIG,
 		fdsProps: {
-			id: `itemSelectorModal-${Date.now()}`,
-			pagination: {
-				deltas: [{label: 20}, {label: 40}, {label: 60}],
-				initialDelta: 20,
-			},
-			views: [
-				{
-					contentRenderer: 'cards',
-					label: Liferay.Language.get('cards'),
-					name: 'cards',
-					schema: {
-						description: 'embedded.description',
-						image: 'embedded.file.thumbnailURL',
-						title: 'embedded.title',
-					},
-					thumbnail: 'cards2',
-				},
-				{
-					contentRenderer: 'table',
-					label: Liferay.Language.get('table'),
-					name: 'table',
-					schema: {
-						fields: [
-							{
-								fieldName: 'embedded.title',
-								label: Liferay.Language.get('title'),
-								sortable: false,
-							},
-							{
-								fieldName: 'embedded.description',
-								label: Liferay.Language.get('description'),
-								sortable: false,
-							},
-							{
-								fieldName: 'embedded.file.name',
-								label: Liferay.Language.get('file-name'),
-								sortable: false,
-							},
-							{
-								fieldName: 'embedded.file.mimeType',
-								label: Liferay.Language.get('type'),
-								sortable: false,
-							},
-						],
-					},
-					thumbnail: 'table',
-				},
-			],
+			...FDS_PROPS,
+			id: `UploadFragmentItemSelectorFDS_${getRandomId()}`,
 		},
 		itemTypeLabel: Liferay.Language.get('files'),
 		items: [],
