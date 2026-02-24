557136456cbd88a6c2ac90ebb0ab530896eb6853
Gabriel Lima <gabriel.lima@liferay.com>
2026-02-24 10:06:30 -0300

    LPD-80034 Replace Playwright test with a more appropriate Java unit test

--- modules/test/playwright/tests/configuration-admin-web/main/configurationAdminUI.spec.ts
+++ modules/test/playwright/tests/configuration-admin-web/main/configurationAdminUI.spec.ts
@@ -11,33 +11,6 @@ import {systemSettingsPageTest} from '../../../fixtures/systemSettingsPageTest';
 export const test = mergeTests(loginTest(), systemSettingsPageTest);
 
 test(
-	'Language keys are properly rendered in the UI',
-	{tag: '@LPD-80034'},
-	async ({page, systemSettingsPage}) => {
-		await test.step('Navigate to Accessibility Menu', async () => {
-			await systemSettingsPage.goToSystemSetting(
-				'Accessibility',
-				'Accessibility Menu'
-			);
-		});
-
-		await test.step('Assert that the title and description are properly rendered', async () => {
-			await expect(
-				page.getByRole('heading', {name: 'Accessibility Menu'})
-			).toBeVisible();
-
-			await expect(
-				page.getByRole('checkbox', {name: 'Enable Accessibility Menu'})
-			).toBeVisible();
-
-			await expect(page.locator('.form-help-text')).toHaveText(
-				'Enable the accessibility menu which can be accessed by tabbing focus to the quick access menu. When enabled, users are able to save their accessibility settings in the browser local storage when not signed in and in the database when signed in.'
-			);
-		});
-	}
-);
-
-test(
 	'Configuration admin UI can autogenerate a doc link',
 	{tag: ['@LPS-194725']},
 	async ({page, systemSettingsPage}) => {
