7e9cb159d896e99edff85f3b19c0e1ea997d83c1
Mario Leandro <mario.leandro@liferay.com>
2026-02-26 11:49:19 -0300

    LPD-80579 Add DesignLibrariesAdminPortletKeys

---
+++ modules/apps/design-libraries/design-libraries-web/src/main/java/com/liferay/design/libraries/web/internal/constants/DesignLibrariesAdminPortletKeys.java
@@ -0,0 +1,17 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.design.libraries.web.internal.constants;
+
+/**
+ * @author Mario Leandro
+ */
+public class DesignLibrariesAdminPortletKeys {
+
+	public static final String DESIGN_LIBRARIES_ADMIN =
+		"com_liferay_design_libraries_web_internal_portlet_" +
+		"DesignLibrariesAdminPortlet";
+
+}
