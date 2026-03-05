924373c5c399b218c37e98e2e16bd5742ca5459f
Mario Leandro <mario.leandro@liferay.com>
2026-03-05 08:16:30 -0300

    LPD-80580 Add FDS Transformer function for Design Library

---
+++ modules/apps/design-library/design-library-web/src/main/resources/META-INF/resources/js/props_transformer/DesignLibraryAdminFDSPropsTransformer.ts
@@ -0,0 +1,53 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+export default function DesignLibraryAdminFDSPropsTransformer(props: any) {
+	const creationMenu = {
+		primaryItems: [
+			{
+				label: Liferay.Language.get('new-design-library'),
+			},
+		],
+	};
+
+	return {
+		...props,
+		creationMenu,
+		hideManagementBarInEmptyState: true,
+		views: [
+			{
+				contentRenderer: 'table',
+				label: 'Table',
+				name: 'table',
+				schema: {
+					fields: [
+						{
+							actionId: 'edit',
+							contentRenderer: 'actionLink',
+							fieldName: 'title',
+							label: Liferay.Language.get('title'),
+							localizeLabel: true,
+							sortable: true,
+						},
+						{
+							fieldName: 'description',
+							label: Liferay.Language.get('description'),
+							localizeLabel: true,
+							truncate: true,
+						},
+						{
+							contentRenderer: 'dateTime',
+							fieldName: 'dateModified',
+							label: Liferay.Language.get('last-updated'),
+							localizeLabel: true,
+							sortable: true,
+						},
+					],
+				},
+				thumbnail: 'table',
+			},
+		],
+	};
+}
