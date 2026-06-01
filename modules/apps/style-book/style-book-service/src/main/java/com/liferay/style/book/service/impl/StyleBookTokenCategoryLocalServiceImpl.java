33b4a3591d4b755b8d2021427d4425434f0bb663
Gabriel Lima <gabriel.lima@liferay.com>
2026-06-01 14:07:52 -0300

    LPD-88085 Add StyleBookTokenCategory create service logic
    Build-Id: 536fa475.1

--- modules/apps/style-book/style-book-service/src/main/java/com/liferay/style/book/service/impl/StyleBookTokenCategoryLocalServiceImpl.java
+++ modules/apps/style-book/style-book-service/src/main/java/com/liferay/style/book/service/impl/StyleBookTokenCategoryLocalServiceImpl.java
@@ -5,10 +5,27 @@
 
 package com.liferay.style.book.service.impl;
 
+import com.liferay.frontend.token.definition.FrontendTokenCategory;
+import com.liferay.frontend.token.definition.FrontendTokenDefinition;
+import com.liferay.frontend.token.definition.FrontendTokenDefinitionRegistry;
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.model.ModelHintsUtil;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.style.book.exception.DuplicateStyleBookTokenCategoryNameException;
+import com.liferay.style.book.exception.StyleBookTokenCategoryNameException;
+import com.liferay.style.book.model.StyleBookTokenCategory;
 import com.liferay.style.book.service.base.StyleBookTokenCategoryLocalServiceBaseImpl;
 
+import java.util.Date;
+
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Brian Wing Shun Chan
@@ -19,4 +36,95 @@ import org.osgi.service.component.annotations.Component;
 )
 public class StyleBookTokenCategoryLocalServiceImpl
 	extends StyleBookTokenCategoryLocalServiceBaseImpl {
+
+	@Override
+	public StyleBookTokenCategory addStyleBookTokenCategory(
+			long styleBookEntryId, String themeFrontendTokenDefinitionId,
+			String name, String description, ServiceContext serviceContext)
+		throws PortalException {
+
+		User user = _userLocalService.getUser(serviceContext.getUserId());
+
+		_validate(
+			user.getCompanyId(), styleBookEntryId,
+			themeFrontendTokenDefinitionId, name);
+
+		long styleBookTokenCategoryId = counterLocalService.increment(
+			StyleBookTokenCategory.class.getName());
+
+		StyleBookTokenCategory styleBookTokenCategory =
+			styleBookTokenCategoryPersistence.create(styleBookTokenCategoryId);
+
+		styleBookTokenCategory.setGroupId(serviceContext.getScopeGroupId());
+		styleBookTokenCategory.setCompanyId(user.getCompanyId());
+		styleBookTokenCategory.setUserId(user.getUserId());
+		styleBookTokenCategory.setUserName(user.getFullName());
+		styleBookTokenCategory.setCreateDate(
+			serviceContext.getCreateDate(new Date()));
+		styleBookTokenCategory.setModifiedDate(
+			serviceContext.getModifiedDate(new Date()));
+		styleBookTokenCategory.setStyleBookEntryId(styleBookEntryId);
+		styleBookTokenCategory.setThemeFrontendTokenDefinitionId(
+			themeFrontendTokenDefinitionId);
+		styleBookTokenCategory.setName(name);
+		styleBookTokenCategory.setDescription(description);
+
+		return styleBookTokenCategoryPersistence.update(styleBookTokenCategory);
+	}
+
+	private void _validate(
+			long companyId, long styleBookEntryId,
+			String themeFrontendTokenDefinitionId, String name)
+		throws PortalException {
+
+		if (Validator.isNull(name)) {
+			throw new StyleBookTokenCategoryNameException("Name is required");
+		}
+
+		int nameMaxLength = ModelHintsUtil.getMaxLength(
+			StyleBookTokenCategory.class.getName(), "name");
+
+		if (name.length() > nameMaxLength) {
+			throw new StyleBookTokenCategoryNameException(
+				"Maximum length of name exceeded");
+		}
+
+		StyleBookTokenCategory styleBookTokenCategory =
+			styleBookTokenCategoryPersistence.fetchBySBEI_TFTDI_N(
+				styleBookEntryId, themeFrontendTokenDefinitionId, name);
+
+		if (styleBookTokenCategory != null) {
+			throw new DuplicateStyleBookTokenCategoryNameException(
+				"Style book token category name \"" + name +
+					"\" already exists");
+		}
+
+		FrontendTokenDefinition frontendTokenDefinition =
+			_frontendTokenDefinitionRegistry.getFrontendTokenDefinition(
+				companyId, themeFrontendTokenDefinitionId);
+
+		if (frontendTokenDefinition == null) {
+			return;
+		}
+
+		for (FrontendTokenCategory frontendTokenCategory :
+				frontendTokenDefinition.getFrontendTokenCategories()) {
+
+			JSONObject jsonObject = frontendTokenCategory.getJSONObject(
+				LocaleUtil.getSiteDefault());
+
+			if (name.equals(jsonObject.getString("name"))) {
+				throw new DuplicateStyleBookTokenCategoryNameException(
+					"Style book token category name \"" + name +
+						"\" already exists");
+			}
+		}
+	}
+
+	@Reference
+	private FrontendTokenDefinitionRegistry _frontendTokenDefinitionRegistry;
+
+	@Reference
+	private UserLocalService _userLocalService;
+
 }
--- modules/apps/style-book/style-book-service/src/main/java/com/liferay/style/book/service/impl/StyleBookTokenCategoryServiceImpl.java
+++ modules/apps/style-book/style-book-service/src/main/java/com/liferay/style/book/service/impl/StyleBookTokenCategoryServiceImpl.java
@@ -6,9 +6,18 @@
 package com.liferay.style.book.service.impl;
 
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.style.book.constants.StyleBookActionKeys;
+import com.liferay.style.book.constants.StyleBookConstants;
+import com.liferay.style.book.model.StyleBookEntry;
+import com.liferay.style.book.model.StyleBookTokenCategory;
+import com.liferay.style.book.service.StyleBookEntryLocalService;
 import com.liferay.style.book.service.base.StyleBookTokenCategoryServiceBaseImpl;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Brian Wing Shun Chan
@@ -22,4 +31,31 @@ import org.osgi.service.component.annotations.Component;
 )
 public class StyleBookTokenCategoryServiceImpl
 	extends StyleBookTokenCategoryServiceBaseImpl {
+
+	@Override
+	public StyleBookTokenCategory addStyleBookTokenCategory(
+			long styleBookEntryId, String themeFrontendTokenDefinitionId,
+			String name, String description, ServiceContext serviceContext)
+		throws PortalException {
+
+		StyleBookEntry styleBookEntry =
+			_styleBookEntryLocalService.getStyleBookEntry(styleBookEntryId);
+
+		_portletResourcePermission.check(
+			getPermissionChecker(), styleBookEntry.getGroupId(),
+			StyleBookActionKeys.MANAGE_STYLE_BOOK_ENTRIES);
+
+		return styleBookTokenCategoryLocalService.addStyleBookTokenCategory(
+			styleBookEntryId, themeFrontendTokenDefinitionId, name, description,
+			serviceContext);
+	}
+
+	@Reference(
+		target = "(resource.name=" + StyleBookConstants.RESOURCE_NAME + ")"
+	)
+	private PortletResourcePermission _portletResourcePermission;
+
+	@Reference
+	private StyleBookEntryLocalService _styleBookEntryLocalService;
+
 }
