bf09163087083922ff113a4d6bcc7110aae6b483
Gabriel Prates <gabsprates@gmail.com>
2026-08-11 15:07:03 +0200

    LPD-98002 Fix panel category order
    The name disambiguates the portlet from analytics. Renaming through the
    portlet title key keeps the sidebar, the Applications Home, the page
    header, and the Roles permission UI in agreement.

--- modules/apps/product-navigation/product-navigation-control-panel/src/main/java/com/liferay/product/navigation/control/panel/internal/application/list/ApplicationsDeveloperIntegrationPanelCategory.java
+++ modules/apps/product-navigation/product-navigation-control-panel/src/main/java/com/liferay/product/navigation/control/panel/internal/application/list/ApplicationsDeveloperIntegrationPanelCategory.java
@@ -21,7 +21,7 @@ import org.osgi.service.component.annotations.Reference;
 @Component(
 	property = {
 		"panel.category.key=" + PanelCategoryKeys.APPLICATIONS_MENU_APPLICATIONS,
-		"panel.category.order:Integer=600"
+		"panel.category.order:Integer=650"
 	},
 	service = PanelCategory.class
 )
